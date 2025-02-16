934f08d5ca98680c8312418366a368fcc2b2e314
khom19 <khom2548@gmail.com>
2025-02-16 21:25:10 +0700

    add hover effect

--- src/Login/loginpage.tsx
+++ src/Login/loginpage.tsx
@@ -26,17 +26,17 @@ const LoginPage: React.FC = () => {
                     </div>
                 </div>
                 <div className="flex flex-col w-full items-center justify-center mt-3 gap-3">
-                    <button className="p-3 pl-6 pr-6 bg-[#E7AB79] rounded-3xl text-2xl font-semibold">Login</button>
+                    <button className="p-3 pl-6 pr-6 bg-[#E7AB79] rounded-3xl text-2xl font-semibold hover:opacity-85">Login</button>
                     <p className="text-xl">or</p>
                     <div className="flex items-center justify-center w-full">
-                        <button className="relative flex items-center justify-center p-3 pl-12 pr-3 bg-transparent rounded-3xl border border-black 
-                                           text-xl font-semibold w-fitcontent">
+                        <button className="relative flex items-center justify-center p-3 pl-12 pr-3 bg-gray-100 rounded-3xl border border-black 
+                                           text-xl font-semibold w-fitcontent hover:bg-gray-200">
                             <FaGoogle className="absolute left-4 top-1/2 transform -translate-y-1/2 text-black-600" size={25} />Sign in with Google
                         </button>
                     </div>
                     <div className="flex flex-row w-full justify-center mb-3 gap-12">
-                        <button className="text-md underline">Create Account</button>
-                        <button className="text-md underline">Forget Password</button>
+                        <button className="text-md underline hover:text-gray-600">Create Account</button>
+                        <button className="text-md underline hover:text-gray-600">Forget Password</button>
                     </div>
                 </div>
             </div>
