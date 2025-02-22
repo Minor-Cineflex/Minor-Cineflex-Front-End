2bcedb88b0584dba01ebca3fa19ebaa8af6d57f6
khom19 <khom2548@gmail.com>
2025-02-22 23:08:53 +0700

    start making Sing Up page

---
+++ src/Login/createaccount.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import Logo from "./logo/MinorCineflexLogo.jpg"
+import { MdEmail } from "react-icons/md";
+import { BsPersonCircle } from "react-icons/bs";
+import { RiKey2Fill } from "react-icons/ri";
+import { FaGoogle, FaChevronLeft } from "react-icons/fa";
+import { useNavigate } from "react-router";
+
+const Create:React.FC = () => {
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState(String)
+    const [username, setUsername] = useState(String)
+    const [password, setPassword] = useState(String)
+    const [confirmPassword, setConfirmPassword] = useState(String)
+
+    const signUp = () => {
+        !email ? alert("Please set your email"):
+        !email.includes("@") || !email.includes(".com") ? alert("Please enter a valid email address"):
+        !username ? alert("Please set your username"):
+        !password ? alert("Please set your password"):
+        password !== confirmPassword ? alert("Passwords don't match"):
+        alert("ok")
+    }
+
+    return(
+        <div className="min-h-screen bg-[#4C3A51] flex items-center justify-center">
+            <div className="relative min-w-80 min-h-80 w-3/12 h-4/12 bg-[#D9D9D9] flex flex-col items-center justify-center rounded-2xl gap-3">
+                <nav onClick={() => navigate("/Login")}><FaChevronLeft className="absolute top-4 left-4 cursor-pointer" size={25}/></nav>
+                <img src={Logo} alt="" className="size-32"/>
+                <div className="flex flex-col items-center justify-center gap-3">
+                    <div className="relative w-full">
+                        <MdEmail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black-500" size={25} />
+                        <input  type="email" 
+                                className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
+                                            rounded-3xl py-2 pl-12 pr-6 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 
+                                            focus:ring-1 sm:text-xl" 
+                                placeholder="Email"
+                                onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="relative w-full">
+                        <BsPersonCircle className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black-500" size={25} />
+                        <input  type="text" 
+                                className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
+                                            rounded-3xl py-2 pl-12 pr-6 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 
+                                            focus:ring-1 sm:text-xl" 
+                                placeholder="Username"
+                                onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="relative w-full">
+                        <RiKey2Fill className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black-500" size={25} />
+                        <input  type="password" 
+                                className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
+                                            rounded-3xl py-2 pl-12 pr-6 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 
+                                            focus:ring-1 sm:text-xl" 
+                                placeholder="Password"
+                                onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="relative w-full">
+                        <RiKey2Fill className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black-500" size={25} />
+                        <input  type="password" 
+                                className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
+                                            rounded-3xl py-2 pl-12 pr-6 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 
+                                            focus:ring-1 sm:text-xl" 
+                                placeholder="Confirm password"
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col w-full items-center justify-center mt-3 gap-3">
+                    <button className="p-3 pl-6 pr-6 bg-[#E7AB79] rounded-3xl text-2xl font-semibold hover:opacity-85" onClick={() => signUp()}>Sign Up</button>
+                    <p className="text-xl">or</p>
+                    <div className="flex items-center justify-center w-full">
+                        <button className="relative flex items-center justify-center p-3 pl-12 pr-3 bg-gray-100 rounded-3xl border border-black 
+                                            text-xl font-semibold w-fitcontent hover:bg-gray-200 mb-6">
+                            <FaGoogle className="absolute left-4 top-1/2 transform -translate-y-1/2 text-black-600" size={25} />Sign in with Google
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Create
--- src/Login/loginpage.tsx
+++ src/Login/loginpage.tsx
@@ -3,8 +3,11 @@ import Logo from "./logo/MinorCineflexLogo.jpg";
 import { BsPersonCircle } from "react-icons/bs";
 import { RiKey2Fill } from "react-icons/ri";
 import { FaGoogle } from "react-icons/fa";
+import { useNavigate } from "react-router";
 
 const LoginPage: React.FC = () => {
+    const navigate = useNavigate()
+
     return(
         <div className="min-h-screen bg-[#4C3A51] flex items-center justify-center">
             <div className="min-w-80 min-h-80 w-3/12 h-4/12 bg-[#D9D9D9] flex flex-col items-center justify-center rounded-2xl gap-3">
@@ -12,7 +15,7 @@ const LoginPage: React.FC = () => {
                 <div className="flex flex-col items-center justify-center gap-3">
                     <div className="relative w-full">
                         <BsPersonCircle className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black-500" size={25} />
-                        <input type="email" className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
+                        <input type="text" className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
                                                     rounded-3xl py-2 pl-12 pr-6 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 
                                                     focus:ring-1 sm:text-xl" placeholder="Username"
                         />
@@ -35,8 +38,8 @@ const LoginPage: React.FC = () => {
                         </button>
                     </div>
                     <div className="flex flex-row w-full justify-center mb-3 gap-12">
-                        <button className="text-md underline hover:text-gray-600">Create Account</button>
-                        <button className="text-md underline hover:text-gray-600">Forget Password</button>
+                        <nav className="text-md underline hover:text-gray-600 cursor-pointer" onClick={() => navigate("/Create")}>Create Account</nav>
+                        <nav className="text-md underline hover:text-gray-600">Forget Password</nav>
                     </div>
                 </div>
             </div>
