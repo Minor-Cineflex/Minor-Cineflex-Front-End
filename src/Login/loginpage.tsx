21b644405c81dec760aebc2f2bbd25baee21fa48
khom19 <khom2548@gmail.com>
2025-02-24 23:14:46 +0700

    make log in with email and password

--- src/Login/loginpage.tsx
+++ src/Login/loginpage.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./logo/MinorCineflexLogo.jpg";
-import { BsPersonCircle } from "react-icons/bs";
 import { RiKey2Fill } from "react-icons/ri";
 import { FaGoogle } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 import { useNavigate } from "react-router";
 
 const LoginPage: React.FC = () => {
     const navigate = useNavigate()
+    const [email, setEmail] = useState(String)
+    const [password, setPassword] = useState(String)
+
+    const Login = async() => {
+        try{
+            const person_list_response = await fetch("http://localhost:8000/minorcineflex/person", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            if(!person_list_response.ok){
+                console.log("Fail to fetch person_list")
+            }
+            const lowercase_email = email.toLocaleLowerCase()
+            const person_list = await person_list_response.json()
+            const user = person_list.find(p => p.email === lowercase_email);
+            if (!user) {
+                alert("This account does not exist")
+                return
+            }
+            if(user.account.password !== password){
+                alert("Incorrect password")
+                return
+            }
+            alert("You can login");
+        }catch(error){
+            console.error("Error to Login:", error);
+            alert("Failed to Login. Please try again.");
+        }
+    }
 
     return(
         <div className="min-h-screen bg-[#4C3A51] flex items-center justify-center">
@@ -14,22 +45,33 @@ const LoginPage: React.FC = () => {
                 <img src={Logo} alt="" className="size-32"/>
                 <div className="flex flex-col items-center justify-center gap-3">
                     <div className="relative w-full">
-                        <BsPersonCircle className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black-500" size={25} />
-                        <input type="text" className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
-                                                    rounded-3xl py-2 pl-12 pr-6 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 
-                                                    focus:ring-1 sm:text-xl" placeholder="Username"
+                        <MdEmail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black-500" size={25} />
+                        <input  type="text" 
+                                className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
+                                            rounded-3xl py-2 pl-12 pr-6 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 
+                                            focus:ring-1 sm:text-xl" 
+                                placeholder="Email"
+                                onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
                     <div className="relative w-full">
                         <RiKey2Fill className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black-500" size={25} />
-                        <input type="password" className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
-                                                    rounded-3xl py-2 pl-12 pr-6 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 
-                                                    focus:ring-1 sm:text-xl" placeholder="Password"
+                        <input  type="password" 
+                                className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
+                                            rounded-3xl py-2 pl-12 pr-6 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 
+                                            focus:ring-1 sm:text-xl"
+                                placeholder="Password"
+                                onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
                 </div>
                 <div className="flex flex-col w-full items-center justify-center mt-3 gap-3">
-                    <button className="p-3 pl-6 pr-6 bg-[#E7AB79] rounded-3xl text-2xl font-semibold hover:opacity-85">Login</button>
+                    <button 
+                        className="p-3 pl-6 pr-6 bg-[#E7AB79] rounded-3xl text-2xl font-semibold hover:opacity-85"
+                        onClick={() => Login()}
+                    >
+                        Login
+                    </button>
                     <p className="text-xl">or</p>
                     <div className="flex items-center justify-center w-full">
                         <button className="relative flex items-center justify-center p-3 pl-12 pr-3 bg-gray-100 rounded-3xl border border-black 
