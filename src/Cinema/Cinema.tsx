d4eb7c201cef22509ffeeec18bd5a410ce97380e
Boonwarat <Boonwarat074@gmail.com>
2025-03-07 22:25:09 +0700

    add header,footer and redirect to login if no user_id

--- src/Cinema/Cinema.tsx
+++ src/Cinema/Cinema.tsx
@@ -5,6 +5,8 @@ import tmpData from "./test2.json";
 import logo from "./MinorCineflexLogo.jpg"
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom"
+import Footerbar from "../component/footerbar/footerbar.tsx";
+import Headerbar from "../component/header/headerbar.tsx";
 
 const region_list = ["กรุงเทพและปริมณฑล","ภาคกลาง","ภาคเหนือ","ภาคใต้","ภาคตะวันออกเฉียงเหนือ","ภาคตะวันออก","ภาคตะวันตก"];
 
@@ -20,9 +22,16 @@ const Region: React.FC = () => {
     console.log(state)
     
 
+
+
+
     const click_cinema = (cinema_id) => {
         console.log(cinema_id)
-        navigate("/Theater", { state: { ...state, cinema_id } });
+        const statedata = state && Object.keys(state).length > 0 ? 
+        { state: { user_id: state, cinema_id } } : 
+        { state: { cinema_id } };
+
+        navigate("/Theater", statedata);
     }
 
     useEffect(() => {
@@ -110,10 +119,8 @@ const CinemaPage: React.FC = () => {
 
     return (
         <div id="headerNav" className="min-h-screen bg-[#4C3A51] flex flex-col items-center">
-            <header className={`bg-white text-black h-20 fixed top-0 w-full flex items-center justify-center shadow-md z-10 transition-transform duration-500 ${isNavbarVisible ? "translate-y-0" : "-translate-y-full"}`}>
-                <nav className="container mx-auto text-center text-lg font-semibold ">
-                    This is header Nav
-                </nav>
+            <header className={`bg-white text-black h-16 fixed top-0 w-full flex items-center justify-center shadow-md z-10 transition-transform duration-500 ${isNavbarVisible ? "translate-y-0" : "-translate-y-full"}`}>
+                <Headerbar/>
             </header>
             <div id = "body" className="flex flex-col w-full justify-center">
                 <div id="่easteregg" className="p-7 whitespace-nowrap justify-center text-[#4C3A51]">
@@ -139,9 +146,7 @@ const CinemaPage: React.FC = () => {
                 {isFooterVisible ? "Hide" : "Show"}
             </button>
             <footer id="footerNav" className={`bg-[#B25068] text-white h-20 fixed bottom-0 w-full flex items-center justify-center transition-transform duration-500 ${isFooterVisible ? "translate-y-0" : "translate-y-full"}`}>
-                <div className="container mx-auto text-center">
-                    <div className="text-black text-3xl font-semibold flex translate-x-10">BUY TICKET</div>
-                </div>
+                <Footerbar/>
             </footer>
         </div>
     );
